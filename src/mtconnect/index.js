a618c7a31c74c70daee617b7788ad49af5f2e4e5
dzakharov90 <dzakharov@yulsip.com>
2023-01-12 18:33:14 +0000

    App Created

---
+++ src/mtconnect/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+var MikroNode = require('mikronode-ng2');
+var connection = MikroNode.getConnection(process.env.MIKROTIK_ADDR,process.env.MIKROTIK_API_USER,process.env.MIKROTIK_API_PASS);
+const conn = connection.connect();
+var chan=conn.openChannel();
+
+
+module.exports.fw_block_ip = function(ip,comment) {
+    chan.write(['/ip/firewall/address-list/add',`=address=${ip}`,`=timeout=${process.env.FWBLOCK_TIMEOUT}`,`=comment=${comment}`,'=list=Blocked'])
+}
