263fe7635ee01485bb270cd4d4099fee6b02b2f9
Dmitriy <42529397+dzakharov90@users.noreply.github.com>
2023-01-15 13:42:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+require('dotenv').config()
 var mt = require('./src/mtconnect/index');
 Tail = require('tail').Tail;
 tail = new Tail("/var/log/syslog");
-require('dotenv').config()
 
 const localip = process.env.EXTERNAL_ADDR;
 const rulessid = process.env.RULES_SIDS.split(",");
 
 try {
     tail.on("line", data => {
-        if (data.match(/[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z]+ [A-Za-z]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] ([A-Za-z0-9 ]+) \[[A-Za-z0-9: ]+\] \[[A-Za-z0-9: ]+\] \{[A-Za-z0-9]+\} ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> [\d]+\.[\d]+\.[\d]+\.[\d]+\:[\d]+/gm)) {
-            console.log(data);
-            const RuleMatch = /[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z]+ [A-Za-z]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] ([A-Za-z0-9 ]+) \[[A-Za-z0-9: ]+\] \[[A-Za-z0-9: ]+\] \{[A-Za-z0-9]+\} ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> [\d]+\.[\d]+\.[\d]+\.[\d]+\:[\d]+/gm.exec(data);
+        if (data.match(/[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z ]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] (.+)\[.+\] \{.+\} ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> [\d]+\.[\d]+\.[\d]+\.[\d]+\:[\d]+/gm)) {
+            //console.log(data);
+            const RuleMatch = /[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z ]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] (.+)\[.+\] \{.+\} ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> [\d]+\.[\d]+\.[\d]+\.[\d]+\:[\d]+/gm.exec(data);
             if (RuleMatch[3] != null || RuleMatch != '') {
                 if (RuleMatch[3] != localip) {
-                    // console.log('Rule SID: ' + RuleMatch[1])
-                    // console.log('Rule : ' + RuleMatch[2])
-                    // console.log('SRC IP: ' + RuleMatch[3])
+                    console.log('Rule SID: ' + RuleMatch[1])
+                    console.log('Rule : ' + RuleMatch[2])
+                    console.log('SRC IP: ' + RuleMatch[3])
                     const blockip = [...rulessid].find(element => element % RuleMatch[1] === 0)
-                    // console.log(blockip)
+                    console.log(blockip)
                     if (blockip) {
                         mt.fw_block_ip(RuleMatch[3],RuleMatch[2])
                         console.log('IP ' + RuleMatch[3] + ' Banned for ' + RuleMatch[2])
