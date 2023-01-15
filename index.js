4c1deaf6688c1bcb39288bce1b38c1673df3ba80
Dmitriy <42529397+dzakharov90@users.noreply.github.com>
2023-01-15 16:53:16 +0300

    update rule logic

--- index.js
+++ index.js
@@ -4,38 +4,27 @@ Tail = require('tail').Tail;
 tail = new Tail("/var/log/syslog");
 
 const localip = process.env.EXTERNAL_ADDR;
-const rulessid = process.env.RULES_SIDS.split(",");
 
-try {
-    tail.on("line", data => {
-        if (data.match(/[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z ]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] (.+) ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+/gm)) {
-            //console.log(data);
-            const RuleMatch = /[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z ]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] (.+) ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+/gm.exec(data);
-            if (RuleMatch[3] != null || RuleMatch != '') {
-                if (RuleMatch[3] != localip) {
-                    console.log('Rule SID: ' + RuleMatch[1])
-                    console.log('Rule : ' + RuleMatch[2])
-                    console.log('SRC IP: ' + RuleMatch[3])
-                    const blockip = [...rulessid].find(element => element % RuleMatch[1] === 0)
-                    console.log(blockip)
-                    if (blockip) {
-                        mt.fw_block_ip(RuleMatch[3],RuleMatch[2])
-                        console.log('IP ' + RuleMatch[3] + ' Banned for ' + RuleMatch[2])
-                    }
-                } else {
-                    console.log('Rule SID: ' + RuleMatch[1])
-                    console.log('Rule : ' + RuleMatch[2])
-                    console.log('DST IP: ' + RuleMatch[4])
-                    const blockip = [...rulessid].find(element => element % RuleMatch[1] === 0)
-                    console.log(blockip)
-                    if (blockip) {
-                        mt.fw_block_ip(RuleMatch[4],RuleMatch[2])
-                        console.log('IP ' + RuleMatch[4] + ' Banned for ' + RuleMatch[2])
-                    }
-                }
-            } 
-        };
-    });
-} catch (ex) {
-    console.log(ex)
-}
+tail.on("line", data => {
+    if (data.match(/[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z ]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] (.+) ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+/gm)) {
+        //console.log(data);
+        const RuleMatch = /[A-Za-z]+ [\d]+ [\d]+\:[\d]+\:[\d]+ [A-Za-z ]+\[[\d]+]\: \[[\d]+\:([\d]+)\:[\d]+\] (.+) ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+ \-\> ([\d]+\.[\d]+\.[\d]+\.[\d]+)\:[\d]+/gm.exec(data);
+        if (RuleMatch[3] != localip) {
+            console.log('Rule SID: ' + RuleMatch[1])
+            console.log('Rule : ' + RuleMatch[2])
+            console.log('SRC IP: ' + RuleMatch[3])
+            if (RuleMatch[2].match(process.env.RULE_MATCH_REGEXP)) {
+                mt.fw_block_ip(RuleMatch[3],RuleMatch[2])
+                console.log('IP ' + RuleMatch[3] + ' Banned for ' + RuleMatch[2])
+            }
+        } else {
+            console.log('Rule SID: ' + RuleMatch[1])
+            console.log('Rule : ' + RuleMatch[2])
+            console.log('DST IP: ' + RuleMatch[4])
+            if (RuleMatch[2].match(process.env.RULE_MATCH_REGEXP)) {
+                mt.fw_block_ip(RuleMatch[4],RuleMatch[2])
+                console.log('IP ' + RuleMatch[4] + ' Banned for ' + RuleMatch[2])
+            }
+        }
+    };
+});
